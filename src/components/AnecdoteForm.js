86e59288088defb26612e94aa9de2732f4375dd2
Haarni Atte J <atte.h.1998@gmail.com>
2018-03-11 18:37:08 +0200

    ..m ja myös notifikaatio kun lisätään blogi

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { anecdoteCreation } from '../reducers/anecdoteReducer'
+import { notify } from '../reducers/notificationReducer'
 class AnecdoteForm extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
-    console.log('Content:', content)
     this.props.store.dispatch(anecdoteCreation(content))
-
+    this.props.store.dispatch(notify('created anecdote "' + content + '"'))
     e.target.anecdote.value = ''
+    setTimeout(() => {
+      this.props.store.dispatch(notify(''))
+    }, 5000)
   }
   render() {
     return (
